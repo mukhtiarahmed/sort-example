e17afe6c9e94da7e53812fea2ec7a14c0e60538b
mukhtiarahmed <mukhtiar.sherazi@gmail.com>
2020-05-27 19:43:20 +0500

    update the code

--- src/main/java/com/tenpearls/sort/SpecialSort.java
+++ src/main/java/com/tenpearls/sort/SpecialSort.java
@@ -1,6 +1,5 @@
 package com.tenpearls.sort;
 
-import java.util.Arrays;
 
 public class SpecialSort {
 
@@ -8,38 +7,35 @@ public class SpecialSort {
      * Odd numbers are sorted before even numbers, and relative order is preserved
      * @param numbers
      */
-    public static void sortOddEven(int numbers[]) {
+     public static void sortOddEven(int numbers[]) {
 
-        if(null == numbers || 0 == numbers.length) return;
+        if (null == numbers || numbers.length <= 1) return;
 
         int length = numbers.length;
-        int left = 0;
-        int right =  length - 1;
-        int rightCount = 0;
-        do {
-
-
-            while (numbers[right] % 2 == 0 && left < right) {
-                right--;
-                rightCount++;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (compare(numbers[j], numbers[j + 1]) > 0) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
             }
-
-
-            while (left < length && numbers[left] % 2 == 1) {
-                left++;
-            }
-
-
-            if (left < right) {
-                int temp = numbers[left];
-                numbers[left] = numbers[right];
-                numbers[right] = temp;
-            }
-        } while (left < right);
-
-        Arrays.sort(numbers, 0, length - rightCount);
-        Arrays.sort(numbers, length - rightCount,  length);
+        }
 
     }
 
-}
+    private static int compare(int o1, int o2) {
+
+        if ((o1 % 2 == 0 && o2 % 2 == 0) || (o1 % 2 == 1 && o2 % 2 == 1))
+            return o1 - o2;
+
+        if (o1 % 2 == 0)
+            return 1;
+        else {
+            return  -1;
+        }
+
+    }
+
+
+};
